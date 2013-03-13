40c911586bda683d7f9adf4579647ca8423d06ff
Ahmed Soror <aasoror@gmail.com>
2013-03-12 20:43:41 -0400

    fix user background not retained after reboot

--- etc/ims_clean.sh
+++ etc/ims_clean.sh
@@ -2,7 +2,11 @@
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
 
+mv /usr/local/etc/dvdplayer/mybg.png /usr/local/etc/dvdplayer/mybg._png
 rm /usr/local/etc/dvdplayer/*.png
+mv /usr/local/etc/dvdplayer/mybg._png /usr/local/etc/dvdplayer/mybg.png
+#rm 'ls /usr/local/etc/dvdplayer/*.png | grep -v 'mybg.png''
+#rm 'ls *.png | grep -v 'mybg.png''
 rm /usr/local/etc/dvdplayer/*.gif
 rm /usr/local/etc/dvdplayer/*.rss
 rm /usr/local/etc/dvdplayer/*.jpg
